0f609c43cc770fadcabb0f9cffaf2bb880cbc160
Iulian Goia <goia.iulian@yahoo.com>
2019-07-25 08:02:21 +0300

    second commit

---
+++ Trial/Debug/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.d
@@ -0,0 +1,83 @@
+Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.o: \
+ ../Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ D:/Workspace/Trial/Inc/stm32f7xx_hal_conf.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Include/core_cm7.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
+ D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+D:/Workspace/Trial/Inc/stm32f7xx_hal_conf.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Include/core_cm7.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/Workspace/Trial/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h:
+
+D:/Workspace/Trial/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
